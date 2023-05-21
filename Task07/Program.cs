539a716c6c3051a2c30f5e3ba2e39b2c63f77a57
Ilya Ladnyuk <finra@mail.ru>
2023-05-21 18:35:07 +0400

    Task07 solved

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"{lastDigit} ");
+}
+else Console.WriteLine("Неверное число!");
