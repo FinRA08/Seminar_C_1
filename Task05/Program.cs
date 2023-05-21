a84ba432b63be973e0f2f41fa38f9081c598e2c4
Ilya Ladnyuk <finra@mail.ru>
2023-05-21 18:20:53 +0400

    int i = -number;

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,9 +1,17 @@
 ﻿Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number < 1)
+{
+    Console.WriteLine("Неккоректный ввод");
+}
+else
+{
 int i = -number;
 while (i <= number)
 {
     Console.Write($"{i} ");
     i++;
 }
+}
+
