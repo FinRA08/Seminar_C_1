f057d3b45c82a95281e819e775783131ac14e0e3
Ilya Ladnyuk <finra@mail.ru>
2023-05-22 09:13:43 +0400

    Task08 solved

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число - программа покажет все четные числа от 1 до введеного Вами числа");
+Console.Write("Введите положительное число > 1 - ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (number > 1)
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+    else count++;
+    }
+}
+else Console.WriteLine("Некоректный ввод, число должно быть > 1");
