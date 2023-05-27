c8d6105475ed6d03a39588c4e65ad0aff9f90f3d
Ilya Ladnyuk <finra@mail.ru>
2023-05-27 09:15:53 +0400

    Task13 solved

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая 
+выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.Write("Введите число и программа выведет третью цифру заданного числа или сообщит что третьей цифры нет: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100)
+{
+    int myresalt = IsolationOfTheThirdNumber(number);
+    Console.WriteLine($"Третье число числа - {number}, это - {myresalt}");
+}
+else Console.WriteLine("Третьей цифры нет");
+
+int IsolationOfTheThirdNumber (int num)
+{
+    if (num >= 100)
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    return num;
+}
