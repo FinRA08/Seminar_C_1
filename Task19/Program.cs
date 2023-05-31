100b9dd784a9eb8c45d7f2534e33f87ea503f49a
Ilya Ladnyuk <finra@mail.ru>
2023-05-31 07:53:59 +0400

    Added Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход пятизначное число 
+// 2. проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Enter a five digit number to determine if it is a polydron: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+void DefinitionOnPolydron (int num)
+{
+    if (num >= 10000 && num <= 99999)
+    {
+        if ((num % 10 == num / 10000) && (num % 100 / 10) == (num / 1000 %10))
+        {
+            Console.WriteLine("да");
+        }else Console.WriteLine("нет");
+    }else Console.WriteLine("an incorrect number has been entered, the number must be five digits");
+}
