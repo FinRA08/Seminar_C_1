7788200bd1b31839e03d189647deca670b1bad63
Ilya Ladnyuk <finra@mail.ru>
2023-05-31 08:05:29 +0400

    Task19 solved

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,15 +8,14 @@
 Console.Write("Enter a five digit number to determine if it is a polydron: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-
+DefinitionOnPolydron(number);
 
 void DefinitionOnPolydron (int num)
 {
+    if (num < 0) num = num * -1;
     if (num >= 10000 && num <= 99999)
     {
-        if ((num % 10 == num / 10000) && (num % 100 / 10) == (num / 1000 %10))
-        {
-            Console.WriteLine("да");
-        }else Console.WriteLine("нет");
+        string printLine = (num % 10 == num / 10000 && num % 100 / 10 == num / 1000 % 10) ? "Yes" : "No";
+        Console.WriteLine(printLine);
     }else Console.WriteLine("an incorrect number has been entered, the number must be five digits");
 }
