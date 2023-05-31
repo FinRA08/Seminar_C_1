5eac9bcc95937505e0d004de4efeec94093bdcb1
Ilya Ladnyuk <finra@mail.ru>
2023-05-31 07:11:52 +0400

    Task23 solved

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N) 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Enter a number N to get a table of cube of numbers from 1 to the entered number N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CalculatingACube(number);
+
+void CalculatingACube (int num)
+{
+    if (num > 1)
+    {     
+        int count = 1;
+        int cube = 0;
+        while(count <= num)
+        {
+            cube = count * count * count;
+            Console.WriteLine($"{count} | {cube}");
+            count++;
+        }
+    }else Console.WriteLine("The wrong number is entered, N must be > 0");
+}
