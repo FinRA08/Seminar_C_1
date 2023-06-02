3ccd5cd5cf6b5b8946aa8184294dfc4b7ac73f7b
Ilya Ladnyuk <finra@mail.ru>
2023-06-02 07:05:16 +0400

    Task24 solved

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// 1. принимает на вход число (А)
+// 2. выдаёт сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int resalt = Sumnumbers(number);
+Console.WriteLine(resalt);
+
+
+int Sumnumbers (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;// sum += i
+    } 
+    return sum;
+}
+
