3ca460693aabc13ad77994b4211c26c9cf22f5cb
Ilya Ladnyuk <finra@mail.ru>
2023-06-06 07:58:26 +0400

    Task31 solved

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 31: Задайте массив из 12 элементов, заполненый
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в мвссиве [...] сумма положительных чисел равна 29
+// сумма отрицательных равна -20
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+PrintArray(array);
+int[] sumPositiveNegativElem = GetSumPositiveNegativElem(array);
+PrintArray(sumPositiveNegativElem);
+
+Console.WriteLine($"Сумма положительных элементов = {sumPositiveNegativElem[0]}");
+Console.WriteLine($"Сумма отрицательны элементов = {sumPositiveNegativElem[1]}");
+
+int sumPositiveElem = GetSumPositiveElem(array);
+int sumNegativeElem = GetSumNegativElem(array);
+Console.WriteLine($"Сумма положительных элементов = {sumPositiveElem}");
+Console.WriteLine($"Сумма отрицательны элементов = {sumNegativeElem}");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] GetSumPositiveNegativElem(int[] arr)
+{
+    int sumPositive = 0;
+    int sumNegative = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) sumPositive += arr[i];
+        else sumNegative += arr[i];
+    }
+    
+    return new int[] {sumPositive, sumNegative};
+}
+
+int GetSumPositiveElem(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int GetSumNegativElem(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0) sum += arr[i];
+    }
+    return sum;
+}
