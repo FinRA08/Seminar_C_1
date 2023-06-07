7bc1b69bb496aaac3a74275c05ec466fdeadf28d
Ilya Ladnyuk <finra@mail.ru>
2023-06-07 08:08:44 +0400

    Task35 solved

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+int[] array = CreateArrayRndInt(123, 1, 555);
+PrintArray(array);
+int numberOfValues = CountTwoDig(array);
+Console.WriteLine($"Массив содержит - {numberOfValues} двухзначных чисел");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountTwoDig(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) count += 1;
+    }
+    return count;
+}
+
