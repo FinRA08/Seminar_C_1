8074837731ea9155c981699ea4f4dbd111d7846b
Ilya Ladnyuk <finra@mail.ru>
2023-06-11 10:03:52 +0400

    Task44 solved

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 44: Не используя рекурсию,
+// выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] CreateArrayFibonacci(int size)
+{
+    int[] arr = new int[size];
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i -2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int EnteringNmber()
+{
+    Console.Write("Введите число для вывода чисел Фибонначи в количестве введеного числа: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) return number;
+    else return 0;
+
+}
+
+int[] array = CreateArrayFibonacci(EnteringNmber());
+PrintArray(array);
+
