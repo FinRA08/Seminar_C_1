41f6b6cdc713a604c4f1ae479b4e66a86e4b1d9c
Ilya Ladnyuk <finra@mail.ru>
2023-06-11 07:59:37 +0400

    Task40 solved

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: Напишите программу, которая принимает на вход 
+// три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+//Теорема о неравенстве треугольника: 
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+int EnteringNmber()
+{
+    Console.Write("Введите длину стороны треугольника: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) return number;
+    else return 0;
+
+}
+
+bool SuchTriangleExists(int a, int b, int c)
+{
+    if((a < b + c) && (b < a + c) && (c < a + b)) return true;
+    else return false;
+}
+
+int side1 = EnteringNmber();
+int side2 = EnteringNmber();
+int side3 = EnteringNmber();
+
+if (side1 == 0 || side2 == 0 || side3 == 0)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+Console.WriteLine(SuchTriangleExists(side1, side2, side3) ? "Такой треугольник существует" : "Такого " 
+                    + "треугольника не существует");
