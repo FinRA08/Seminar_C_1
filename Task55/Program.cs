ef8fcd208c59f3da9831dd104435387c2ced4717
Ilya Ladnyuk <finra@mail.ru>
2023-06-17 07:05:44 +0400

    Task55 solved

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -30,10 +30,9 @@ void PrintMatrix (int[,] matrix)
 
 void SwapToColumns (int[,] matrix)
 {
-    int lastRow = matrix.GetLength(0) - 1;
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
     {
-        for (int j = 0; j < i; i++)
+        for (int j = 0; j < i; j++)
         {
             int temp = matrix[i,j];
             matrix[i,j] = matrix[j,i];
@@ -47,7 +46,7 @@ bool IsMatrixSquare(int[,] matrix)
     return matrix.GetLength(0) == matrix.GetLength(1);
 }
 
-int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
 PrintMatrix(array2d);
 Console.WriteLine();
 if (IsMatrixSquare(array2d))
