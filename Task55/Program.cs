2648d33c34dc3cce919a4b8c7e8e8a74d267fcbf
Ilya Ladnyuk <finra@mail.ru>
2023-06-17 06:52:31 +0400

    Added Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 55: Задайте двумерный массив
+// Напишите программу, которая заменяет строки на столбцы
+// В случае если это невозможно, программа должна вынести сообщение для пользователя
+
+int[,] CreateMatrixRndInt (int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int [rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max +1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix [i,j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapToColumns (int[,] matrix)
+{
+    int lastRow = matrix.GetLength(0) - 1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; i++)
+        {
+            int temp = matrix[i,j];
+            matrix[i,j] = matrix[j,i];
+            matrix[j,i] = temp; 
+        }
+    }
+}
+
+bool IsMatrixSquare(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+if (IsMatrixSquare(array2d))
+{
+    SwapToColumns(array2d);
+    PrintMatrix(array2d);
+}
+else Console.WriteLine("Matrix isnt square");
