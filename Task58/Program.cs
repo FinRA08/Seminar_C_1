b50f10dfd63fb5d48b87f8f5e8807fa251bc0ac6
Ilya Ladnyuk <finra@mail.ru>
2023-06-20 08:22:17 +0400

    Task58 solved

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -6,3 +6,56 @@
 // 18 20
 // 15 18
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    int[,] matrixTotal = new int[matrixA.GetLength(0), matrixA.GetLength(1)];
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++) matrixTotal[i, j] += matrixA[i, k] * matrixB[k, j];
+
+        }
+    }
+    return matrixTotal;
+
+}
+
+int[,] array2dA = CreateMatrixRndInt(2, 2, 1, 10);
+PrintMatrix(array2dA);
+Console.WriteLine();
+int[,] array2dB = CreateMatrixRndInt(2, 2, 1, 10);
+PrintMatrix(array2dB);
+Console.WriteLine();
+if (array2dA.GetLength(1) == array2dB.GetLength(0))
+{
+    int[,] totalMatrix = MatrixMultiplication(array2dA, array2dB);
+    PrintMatrix(totalMatrix);
+}
+else Console.WriteLine("Для произведение матриц необходимо что бы число столбцов матрицы А была равна числу строк матрицы В");
