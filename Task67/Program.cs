fdf9a45902f211d768909d22015cada0f51ee82f
Ilya Ladnyuk <finra@mail.ru>
2023-06-22 07:45:37 +0400

    Task67 solved

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -2,4 +2,19 @@
 // 453 -> 12
 // 45 -> 9
 
+int EnterNumber (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits (int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+}
+
+int number = EnterNumber("Введите натуральное число: ");
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма чисел чесла -> {number} = {sumDigits}");
 
