638e6206480e31cdb2ff9d5ea59fd6487644e63f
Ilya Ladnyuk <finra@mail.ru>
2023-06-22 07:02:27 +0400

    Task63 solved

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int EnterNumber (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+int number = EnterNumber("Введите натуральное  число: ");
+NaturalNumbers(number);
