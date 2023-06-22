cc21315374a49d28b458cbcb60451ab9e0f1d290
Ilya Ladnyuk <finra@mail.ru>
2023-06-22 08:18:12 +0400

    Task64 solved

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -2,4 +2,22 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+int EnterNumber (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+void NaturalNumbers(int num)
+{
+    if(num >= 0)
+    {
+        if(num == 0) return;
+        Console.Write($"{num} ");
+        NaturalNumbers(num - 1);
+    }
+    else Console.WriteLine("Введено не коректное число, чило N должно быть > 0");
+}
+
+int number = EnterNumber("Введите натуральное  число: ");
+NaturalNumbers(number);
